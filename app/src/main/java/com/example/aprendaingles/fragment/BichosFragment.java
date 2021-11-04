3c42f6c85bb46164e0b0f36340217239bd967847
Lorrane Alves <Lorrane.dev@gmail.com>
2021-11-04 11:24:33 -0300

    Aba 'Bichos' implementada

--- app/src/main/java/com/example/aprendaingles/fragment/BichosFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragment/BichosFragment.java
@@ -1,5 +1,6 @@
 package com.example.aprendaingles.fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.aprendaingles.R;
 
-public class BichosFragment extends Fragment {
+public class BichosFragment extends Fragment implements View.OnClickListener{
+
+    private ImageButton btnCao, btnGato, btnLeao, btnMacaco, btnOvelha, btnVaca;
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
         // Required empty public constructor
@@ -20,7 +26,62 @@ public class BichosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        btnCao = view.findViewById(R.id.btnCao);
+        btnGato = view.findViewById(R.id.btnGato);
+        btnLeao = view.findViewById(R.id.btnLeao);
+        btnMacaco = view.findViewById(R.id.btnMacaco);
+        btnOvelha = view.findViewById(R.id.btnOvelha);
+        btnVaca = view.findViewById(R.id.btnVaca);
+
+        btnCao.setOnClickListener(this);
+        btnGato.setOnClickListener(this);
+        btnLeao.setOnClickListener(this);
+        btnMacaco.setOnClickListener(this);
+        btnOvelha.setOnClickListener(this);
+        btnVaca.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnCao:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.dog);
+                tocarSom();
+                break;
+            case R.id.btnGato:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cat);
+                tocarSom();
+                break;
+            case R.id.btnLeao:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.lion);
+                tocarSom();
+                break;
+            case R.id.btnMacaco:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.monkey);
+                tocarSom();
+                break;
+            case R.id.btnOvelha:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sheep);
+                tocarSom();
+                break;
+            case R.id.btnVaca:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cow);
+                tocarSom();
+                break;
+        }
+    }
+
+    public void tocarSom(){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(mp -> mediaPlayer.release());
+        }
     }
 }
--- app/src/main/java/com/example/aprendaingles/fragment/NumerosFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragment/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.example.aprendaingles.fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,19 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
 
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener {
 
-    public NumerosFragment() {
-        // Required empty public constructor
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+    public void onClick(View v) {
+
     }
 }
