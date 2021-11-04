4e6b3d34981253721f4d7a54c2b5a4e122e2c537
Lorrane Alves <Lorrane.dev@gmail.com>
2021-11-04 11:33:05 -0300

    Aba 'Numeros' implementada

--- app/src/main/java/com/example/aprendaingles/fragment/NumerosFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragment/NumerosFragment.java
@@ -14,9 +14,71 @@ import com.example.aprendaingles.R;
 
 public class NumerosFragment extends Fragment implements View.OnClickListener {
 
+    private ImageButton btnUm, btnDois, btnTres, btnQuatro, btnCinco, btnSeis;
+    private MediaPlayer mediaPlayer;
+
+    public NumerosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        btnUm = view.findViewById(R.id.btnUm);
+        btnDois = view.findViewById(R.id.btnDois);
+        btnTres = view.findViewById(R.id.btnTres);
+        btnQuatro = view.findViewById(R.id.btnQuatro);
+        btnCinco = view.findViewById(R.id.btnCinco);
+        btnSeis = view.findViewById(R.id.btnSeis);
+
+        btnUm.setOnClickListener(this);
+        btnDois.setOnClickListener(this);
+        btnTres.setOnClickListener(this);
+        btnQuatro.setOnClickListener(this);
+        btnCinco.setOnClickListener(this);
+        btnSeis.setOnClickListener(this);
+
+        return view;
+    }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnUm:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+                tocarSom();
+                break;
+            case R.id.btnDois:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+                tocarSom();
+                break;
+            case R.id.btnTres:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+                tocarSom();
+                break;
+            case R.id.btnQuatro:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+                tocarSom();
+                break;
+            case R.id.btnCinco:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+                tocarSom();
+                break;
+            case R.id.btnSeis:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+                tocarSom();
+                break;
+        }
+    }
 
+    public void tocarSom(){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(mp -> mediaPlayer.release());
+        }
     }
 }
